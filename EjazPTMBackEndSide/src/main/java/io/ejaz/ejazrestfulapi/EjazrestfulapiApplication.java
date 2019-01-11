0b0c2845b34e90dd2feb472abfcdbc275e843332
Waseem Awashra <yzmane@gmail.com>
2019-01-11 16:17:13 +0200

    update .gitignore file

---
+++ EjazPTMBackEndSide/src/main/java/io/ejaz/ejazrestfulapi/EjazrestfulapiApplication.java
@@ -0,0 +1,14 @@
+package io.ejaz.ejazrestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjazrestfulapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjazrestfulapiApplication.class, args);
+    }
+
+}
+
---
+++ EjazPTMBackEndSide/src/test/java/io/ejaz/ejazrestfulapi/EjazrestfulapiApplicationTests.java
@@ -0,0 +1,17 @@
+package io.ejaz.ejazrestfulapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EjazrestfulapiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
