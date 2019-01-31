b0934040679ce1ca93196589e19ae66896546918
Waseem Awashra <yzmane@gmail.com>
2019-01-31 21:41:36 +0200

    Update import Package

--- EjazPTMBackEndSide/src/main/java/io/ejaz/ejazrestfulapi/web/ProjectController.java
+++ EjazPTMBackEndSide/src/main/java/io/ejaz/ejazrestfulapi/web/ProjectController.java
@@ -7,12 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
